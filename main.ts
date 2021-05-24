f398cd35497bc7db2273eae964c73cc7af096e17
palm119 <tmp2tmp@126.com>
2021-05-24 20:10:49 +0800

    初始化版本.

---
+++ main.ts
@@ -0,0 +1,366 @@
+﻿// MakerBit blocks supporting a Keyestudio Infrared Wireless Module Kit
+// (receiver module+remote controller)
+
+const enum IrButton {
+  //% block="1"
+  Number_1 = 0xa2,
+  //% block="2"
+  Number_2 = 0x62,
+  //% block="3"
+  Number_3 = 0xe2,
+  //% block="4"
+  Number_4 = 0x22,
+  //% block="5"
+  Number_5 = 0x02,
+  //% block="6"
+  Number_6 = 0xc2,
+  //% block="7"
+  Number_7 = 0xe0,
+  //% block="8"
+  Number_8 = 0xa8,
+  //% block="9"
+  Number_9 = 0x90,
+  //% block="*"
+  Star = 0x68,
+  //% block="0"
+  Number_0 = 0x98,
+  //% block="#"
+  Hash = 0xb0,
+  //% block=" "
+  Unused_4 = -1,
+  //% block="▲"
+  Up = 0x18,
+  //% block=" "
+  Unused_2 = -2,
+  //% block="◀"
+  Left = 0x10,
+  //% block="OK"
+  Ok = 0x38,
+  //% block="▶"
+  Right = 0x5a,
+  //% block=" "
+  Unused_3 = -3,
+  //% block="▼"
+  Down = 0x4a,
+  //% block="any"
+  Any = -4,
+}
+
+const enum IrButtonAction {
+  //% block="按下"
+  Pressed = 0,
+  //% block="松开"
+  Released = 1,
+}
+
+const enum IrProtocol {
+  //% block="Keyestudio"
+  Keyestudio = 0,
+  //% block="NEC"
+  NEC = 1,
+}
+
+//% color=#0fbc11 icon="\u272a" block="老许红外"
+//% category="MakerBit"
+namespace laoxuir {
+  let irState: IrState;
+
+  const MICROBIT_MAKERBIT_IR_NEC = 777;
+  const MICROBIT_MAKERBIT_IR_DATAGRAM = 778;
+  const MICROBIT_MAKERBIT_IR_BUTTON_PRESSED_ID = 789;
+  const MICROBIT_MAKERBIT_IR_BUTTON_RELEASED_ID = 790;
+  const IR_REPEAT = 256;
+  const IR_INCOMPLETE = 257;
+  const IR_DATAGRAM = 258;
+
+  interface IrState {
+    protocol: IrProtocol;
+    hasNewDatagram: boolean;
+    bitsReceived: uint8;
+    addressSectionBits: uint16;
+    commandSectionBits: uint16;
+    hiword: uint16;
+    loword: uint16;
+  }
+
+  function appendBitToDatagram(bit: number): number {
+    irState.bitsReceived += 1;
+
+    if (irState.bitsReceived <= 8) {
+      irState.hiword = (irState.hiword << 1) + bit;
+      if (irState.protocol === IrProtocol.Keyestudio && bit === 1) {
+        // recover from missing message bits at the beginning
+        // Keyestudio address is 0 and thus missing bits can be detected
+        // by checking for the first inverse address bit (which is a 1)
+        irState.bitsReceived = 9;
+        irState.hiword = 1;
+      }
+    } else if (irState.bitsReceived <= 16) {
+      irState.hiword = (irState.hiword << 1) + bit;
+    } else if (irState.bitsReceived <= 32) {
+      irState.loword = (irState.loword << 1) + bit;
+    }
+
+    if (irState.bitsReceived === 32) {
+      irState.addressSectionBits = irState.hiword & 0xffff;
+      irState.commandSectionBits = irState.loword & 0xffff;
+      return IR_DATAGRAM;
+    } else {
+      return IR_INCOMPLETE;
+    }
+  }
+
+  function decode(markAndSpace: number): number {
+    if (markAndSpace < 1600) {
+      // low bit
+      return appendBitToDatagram(0);
+    } else if (markAndSpace < 2700) {
+      // high bit
+      return appendBitToDatagram(1);
+    }
+
+    irState.bitsReceived = 0;
+
+    if (markAndSpace < 12500) {
+      // Repeat detected
+      return IR_REPEAT;
+    } else if (markAndSpace < 14500) {
+      // Start detected
+      return IR_INCOMPLETE;
+    } else {
+      return IR_INCOMPLETE;
+    }
+  }
+
+  function enableIrMarkSpaceDetection(pin: DigitalPin) {
+    pins.setPull(pin, PinPullMode.PullNone);
+
+    let mark = 0;
+    let space = 0;
+
+    pins.onPulsed(pin, PulseValue.Low, () => {
+       mark = pins.pulseDuration();
+    });
+
+    pins.onPulsed(pin, PulseValue.High, () => {
+      // LOW
+      space = pins.pulseDuration();
+      const status = decode(mark + space);
+
+      if (status !== IR_INCOMPLETE) {
+        control.raiseEvent(MICROBIT_MAKERBIT_IR_NEC, status);
+      }
+    });
+  }
+
+  /**
+   * 指定你红外线接收模块插在开发板的某个数据口，默认P1.
+   * @param pin IR receiver pin, eg: DigitalPin.P1
+   */
+  //% blockId="makerbit_infrared_connect_receiver"
+  //% block="连接红外接收器在 %pin"
+  //% pin.fieldEditor="gridpicker"
+  //% pin.fieldOptions.columns=4
+  //% pin.fieldOptions.tooltips="false"
+  //% weight=90
+  export function connectIrReceiver(
+    pin: DigitalPin
+  ): void {
+    if (irState) {
+      return;
+    }
+
+    irState = {
+      protocol: IrProtocol.Keyestudio,
+      bitsReceived: 0,
+      hasNewDatagram: false,
+      addressSectionBits: 0,
+      commandSectionBits: 0,
+      hiword: 0, // TODO replace with uint32
+      loword: 0,
+    };
+
+    enableIrMarkSpaceDetection(pin);
+
+    let activeCommand = -1;
+    let repeatTimeout = 0;
+    const REPEAT_TIMEOUT_MS = 120;
+
+    control.onEvent(
+      MICROBIT_MAKERBIT_IR_NEC,
+      EventBusValue.MICROBIT_EVT_ANY,
+      () => {
+        const irEvent = control.eventValue();
+
+        // Refresh repeat timer
+        if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+          repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+        }
+
+        if (irEvent === IR_DATAGRAM) {
+          irState.hasNewDatagram = true;
+          control.raiseEvent(MICROBIT_MAKERBIT_IR_DATAGRAM, 0);
+
+          const newCommand = irState.commandSectionBits >> 8;
+
+          // Process a new command
+          if (newCommand !== activeCommand) {
+            if (activeCommand >= 0) {
+              control.raiseEvent(
+                MICROBIT_MAKERBIT_IR_BUTTON_RELEASED_ID,
+                activeCommand
+              );
+            }
+
+            activeCommand = newCommand;
+            control.raiseEvent(
+              MICROBIT_MAKERBIT_IR_BUTTON_PRESSED_ID,
+              newCommand
+            );
+          }
+        }
+      }
+    );
+
+    control.inBackground(() => {
+      while (true) {
+        if (activeCommand === -1) {
+          // sleep to save CPU cylces
+          basic.pause(2 * REPEAT_TIMEOUT_MS);
+        } else {
+          const now = input.runningTime();
+          if (now > repeatTimeout) {
+            // repeat timed out
+            control.raiseEvent(
+              MICROBIT_MAKERBIT_IR_BUTTON_RELEASED_ID,
+              activeCommand
+            );
+            activeCommand = -1;
+          } else {
+            basic.pause(REPEAT_TIMEOUT_MS);
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * 当你按下或松开红外线遥控器某个按钮时触发这个事件.
+   * @param button the button to be checked
+   * @param action the trigger action
+   * @param handler body code to run when the event is raised
+   */
+  //% blockId=makerbit_infrared_on_ir_button
+  //% block="当遥控器按钮 | %button | %action"
+  //% button.fieldEditor="gridpicker"
+  //% button.fieldOptions.columns=3
+  //% button.fieldOptions.tooltips="false"
+  //% weight=50
+  export function onIrButton(
+    button: IrButton,
+    action: IrButtonAction,
+    handler: () => void
+  ) {
+    control.onEvent(
+      action === IrButtonAction.Pressed
+        ? MICROBIT_MAKERBIT_IR_BUTTON_PRESSED_ID
+        : MICROBIT_MAKERBIT_IR_BUTTON_RELEASED_ID,
+      button === IrButton.Any ? EventBusValue.MICROBIT_EVT_ANY : button,
+      () => {
+        handler();
+      }
+    );
+  }
+
+  /**
+   * 返回红外按钮的编码，通过它判断你按下的时那个按钮，如果是没有按钮按下，返回-1.
+   */
+  //% blockId=makerbit_infrared_ir_button_pressed
+  //% block="按下的按钮"
+  //% weight=70
+  export function irButton(): number {
+    basic.pause(0); // Yield to support background processing when called in tight loops
+    if (!irState) {
+      return IrButton.Any;
+    }
+    return irState.commandSectionBits >> 8;
+  }
+
+  /**
+   * 当收到红外数据是触发这个事件，可以通过这个事件来实现具体按钮要实现的功能.
+   * @param handler body code to run when the event is raised
+   */
+  //% blockId=makerbit_infrared_on_ir_datagram
+  //% block="收到红外数据时"
+  //% weight=40
+  export function onIrDatagram(handler: () => void) {
+    control.onEvent(
+      MICROBIT_MAKERBIT_IR_DATAGRAM,
+      EventBusValue.MICROBIT_EVT_ANY,
+      () => {
+        handler();
+      }
+    );
+  }
+
+  /**
+   * Returns the IR datagram as 32-bit hexadecimal string.
+   * The last received datagram is returned or "0x00000000" if no data has been received yet.
+   */
+  export function irDatagram(): string {
+    basic.pause(0); // Yield to support background processing when called in tight loops
+    if (!irState) {
+      return "0x00000000";
+    }
+    return (
+      "0x" +
+      ir_rec_to16BitHex(irState.addressSectionBits) +
+      ir_rec_to16BitHex(irState.commandSectionBits)
+    );
+  }
+
+  /**
+   * Returns true if any IR data was received since the last call of this function. False otherwise.
+   */
+  export function wasIrDataReceived(): boolean {
+    basic.pause(0); // Yield to support background processing when called in tight loops
+    if (!irState) {
+      return false;
+    }
+    if (irState.hasNewDatagram) {
+      irState.hasNewDatagram = false;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * 返回某个红外按钮编码，通过它区别按下了什么按钮.
+   * @param button the button
+   */
+  //% blockId=makerbit_infrared_button_code
+  //% button.fieldEditor="gridpicker"
+  //% button.fieldOptions.columns=3
+  //% button.fieldOptions.tooltips="false"
+  //% block="按钮 %button"
+  //% weight=60
+  export function irButtonCode(button: IrButton): number {
+    basic.pause(0); // Yield to support background processing when called in tight loops
+    return button as number;
+  }
+
+  function ir_rec_to16BitHex(value: number): string {
+    let hex = "";
+    for (let pos = 0; pos < 4; pos++) {
+      let remainder = value % 16;
+      if (remainder < 10) {
+        hex = remainder.toString() + hex;
+      } else {
+        hex = String.fromCharCode(55 + remainder) + hex;
+      }
+      value = Math.idiv(value, 16);
+    }
+    return hex;
+  }
+}
